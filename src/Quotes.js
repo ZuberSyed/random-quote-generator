32278181f77c3136ab061e79cd63a81dfcf9938c
Syed Zuberia <52675240+ZuberSyed@users.noreply.github.com>
2021-10-02 11:02:21 +0530

    Updated

--- src/Quotes.js
+++ src/Quotes.js
@@ -3,14 +3,23 @@ import React from "react";
 export default function Quotes(props) {
   return (
     <div className="quotes">
+      <h1>Inspirational Quotes</h1>
       <div className="quote">
-        <h1>{`"${props.quotes.text}"`}</h1>
+        <h2>{`" ${props.quotes.text} "`}</h2>
         <p>{`--${props.quotes.author}`}</p>
-      </div>
-      <div className="btnStyle">
-        <button className="btn" onClick={props.handleChange}>
-          Get quote
-        </button>
+        <div className="btnStyle">
+          <button className="btn" onClick={props.handleChange}>
+            Get quote
+          </button>
+          <a
+            className="btn"
+            href={`https://twitter.com/intent/tweet/?text= ${props.quotes.text} -- ${props.quotes.author}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tweet
+          </a>
+        </div>
       </div>
     </div>
   );
